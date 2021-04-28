7ff1834c1e9b8e8b6df66b0e9319ddf6762d69ba
BradDesktop <brad.martin@xdisplay.com>
2021-04-28 10:55:19 -0400

    works nicely

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -32,53 +32,169 @@ namespace Stepino
             this.btnHome = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.btnGo = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.portComboBox = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.gbxComm = new System.Windows.Forms.GroupBox();
+            this.gbxMotion = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.gbxComm.SuspendLayout();
+            this.gbxMotion.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(12, 9);
+            this.btnHome.BackColor = System.Drawing.Color.White;
+            this.btnHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Location = new System.Drawing.Point(6, 22);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(75, 23);
             this.btnHome.TabIndex = 0;
             this.btnHome.Text = "Home";
-            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.UseVisualStyleBackColor = false;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(153, 11);
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 52);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             4750,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 23);
             this.numericUpDown1.TabIndex = 1;
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(317, 9);
+            this.btnGo.BackColor = System.Drawing.Color.White;
+            this.btnGo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnGo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGo.Location = new System.Drawing.Point(6, 81);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(75, 23);
             this.btnGo.TabIndex = 2;
             this.btnGo.Text = "Go";
-            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.UseVisualStyleBackColor = false;
             this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.BackColor = System.Drawing.Color.White;
+            this.btnScan.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnScan.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScan.Location = new System.Drawing.Point(6, 22);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(93, 23);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "Scan Ports";
+            this.btnScan.UseVisualStyleBackColor = false;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // portComboBox
+            // 
+            this.portComboBox.FormattingEnabled = true;
+            this.portComboBox.Location = new System.Drawing.Point(6, 51);
+            this.portComboBox.Name = "portComboBox";
+            this.portComboBox.Size = new System.Drawing.Size(93, 23);
+            this.portComboBox.TabIndex = 4;
+            this.portComboBox.SelectedIndexChanged += new System.EventHandler(this.portComboBox_SelectedIndexChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.White;
+            this.btnConnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnConnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Location = new System.Drawing.Point(6, 80);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(93, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.BackColor = System.Drawing.Color.White;
+            this.btnDisconnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnDisconnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDisconnect.Location = new System.Drawing.Point(6, 109);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(93, 23);
+            this.btnDisconnect.TabIndex = 6;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = false;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 262);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(569, 22);
+            this.statusStrip.TabIndex = 8;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(88, 17);
+            this.statusLabel.Text = "No Connection";
+            // 
+            // gbxComm
+            // 
+            this.gbxComm.Controls.Add(this.btnScan);
+            this.gbxComm.Controls.Add(this.portComboBox);
+            this.gbxComm.Controls.Add(this.btnDisconnect);
+            this.gbxComm.Controls.Add(this.btnConnect);
+            this.gbxComm.Location = new System.Drawing.Point(12, 9);
+            this.gbxComm.Name = "gbxComm";
+            this.gbxComm.Size = new System.Drawing.Size(109, 141);
+            this.gbxComm.TabIndex = 9;
+            this.gbxComm.TabStop = false;
+            this.gbxComm.Text = "Communication";
+            // 
+            // gbxMotion
+            // 
+            this.gbxMotion.Controls.Add(this.btnHome);
+            this.gbxMotion.Controls.Add(this.numericUpDown1);
+            this.gbxMotion.Controls.Add(this.btnGo);
+            this.gbxMotion.Location = new System.Drawing.Point(144, 9);
+            this.gbxMotion.Name = "gbxMotion";
+            this.gbxMotion.Size = new System.Drawing.Size(91, 114);
+            this.gbxMotion.TabIndex = 10;
+            this.gbxMotion.TabStop = false;
+            this.gbxMotion.Text = "Motion";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 82);
-            this.Controls.Add(this.btnGo);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.btnHome);
+            this.ClientSize = new System.Drawing.Size(569, 284);
+            this.Controls.Add(this.gbxMotion);
+            this.Controls.Add(this.gbxComm);
+            this.Controls.Add(this.statusStrip);
             this.Name = "FormMain";
             this.Text = "Stepino";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.gbxComm.ResumeLayout(false);
+            this.gbxMotion.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +203,14 @@ namespace Stepino
         private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ComboBox portComboBox;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.GroupBox gbxComm;
+        private System.Windows.Forms.GroupBox gbxMotion;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -1,39 +1,133 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Drawing;
+
+delegate void StringArgReturningVoidDelegate(string text);
 
 namespace Stepino
 {
     public partial class FormMain : Form
     {
-        public static SerialPort _Port = new SerialPort("COM6", 9600);
+        private SerialPort _Port;
 
         public FormMain()
         {
             InitializeComponent();
             FormClosed += new FormClosedEventHandler(Form1_FormClosed);
-            _Port.Open();
+
+            _Port = new SerialPort
+            {
+                BaudRate = 9600,
+                DataBits = 8,
+                DiscardNull = false,
+                DtrEnable = false,
+                RtsEnable = true,
+                Handshake = Handshake.None,
+                ReadBufferSize = 4096,
+                ReadTimeout = -1,
+                WriteTimeout = -1,
+                StopBits = StopBits.One
+            };
+
+            _Port.DataReceived += _Port_DataReceived;
+
+            System.Threading.Thread.Sleep(100);
+            btnScan.PerformClick();
         }
 
         void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (_Port != null && _Port.IsOpen)
-                _Port.Close();
-        }
-
-        private void PortWrite(string message)
-        {
-            _Port.Write(message);
+            _Port.Close();
         }
 
         private void btnHome_Click(object sender, EventArgs e)
         {
-            PortWrite("H");
+            _Port.Write("H");
+            InMotion(true);
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
-            PortWrite("G" + numericUpDown1.Value.ToString());
+            statusLabel.Text = "";
+            InMotion(true);
+            _Port.Write("G" + numericUpDown1.Value.ToString());
+        }
+
+        private void InMotion(bool moving)
+        {
+            gbxMotion.Enabled = !moving;
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            portComboBox.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                portComboBox.Items.Add(s);
+            }
+            portComboBox.SelectedIndex = portComboBox.Items.Count - 1;
+            _Port.PortName = portComboBox.Text;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                statusLabel.Text = "";
+                _Port.Open();
+                System.Threading.Thread.Sleep(100);
+                _Port.Write(" ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            _Port.Close();
+        }
+
+        private void portComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_Port.IsOpen)
+                return;
+
+            _Port.PortName = portComboBox.Text;
+        }
+
+        private void _Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)sender;
+            string msg;
+            msg = serialPort.ReadLine();
+            MsgIn(msg);
+        }
+
+        private void MsgIn(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (statusStrip.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(MsgIn);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                statusLabel.Text = text.Trim();
+                if (text.Contains("OK") || text.Contains("HOMED"))
+                {
+                    InMotion(false);
+
+                    if (text.Contains("HOMED"))
+                        btnHome.BackColor = Color.LawnGreen;
+                }
+            }
         }
     }
 }
